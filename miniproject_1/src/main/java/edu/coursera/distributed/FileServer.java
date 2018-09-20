11b042a622f5ecc9b0b941b93e1653f078477ced
dehasi <rgaleyev@gmail.com>
2018-09-20 15:42:16 +0200

    DPJ-4: Finish task

--- miniproject_1/src/main/java/edu/coursera/distributed/FileServer.java
+++ miniproject_1/src/main/java/edu/coursera/distributed/FileServer.java
@@ -1,5 +1,9 @@
 package edu.coursera.distributed;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.File;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * A basic and very limited implementation of a file server that responds to GET
@@ -17,8 +22,8 @@ public final class FileServer {
      * Main entrypoint for the basic file server.
      *
      * @param socket Provided socket to accept connections on.
-     * @param fs A proxy filesystem to serve files from. See the PCDPFilesystem
-     *           class for more detailed documentation of its usage.
+     * @param fs     A proxy filesystem to serve files from. See the PCDPFilesystem
+     *               class for more detailed documentation of its usage.
      * @param ncores The number of cores that are available to your
      *               multi-threaded file server. Using this argument is entirely
      *               optional. You are free to use this information to change
@@ -28,55 +33,55 @@ public final class FileServer {
      *                     implementation is not expected to ever throw
      *                     IOExceptions during normal operation.
      */
+
+
     public void run(final ServerSocket socket, final PCDPFilesystem fs,
-            final int ncores) throws IOException {
+                    final int ncores) throws IOException {
         /*
          * Enter a spin loop for handling client requests to the provided
          * ServerSocket object.
          */
         while (true) {
 
-            // TODO Delete this once you start working on your solution.
-            throw new UnsupportedOperationException();
 
-            // TODO 1) Use socket.accept to get a Socket object
+            Socket accept = socket.accept();
 
-            /*
-             * TODO 2) Now that we have a new Socket object, handle the parsing
-             * of the HTTP message on that socket and returning of the requested
-             * file in a separate thread. You are free to choose how that new
-             * thread is created. Common approaches would include spawning a new
-             * Java Thread or using a Java Thread Pool. The steps to complete
-             * the handling of HTTP messages are the same as in MiniProject 2,
-             * but are repeated below for convenience:
-             *
-             *   a) Using Socket.getInputStream(), parse the received HTTP
-             *      packet. In particular, we are interested in confirming this
-             *      message is a GET and parsing out the path to the file we are
-             *      GETing. Recall that for GET HTTP packets, the first line of
-             *      the received packet will look something like:
-             *
-             *          GET /path/to/file HTTP/1.1
-             *   b) Using the parsed path to the target file, construct an
-             *      HTTP reply and write it to Socket.getOutputStream(). If the
-             *      file exists, the HTTP reply should be formatted as follows:
-             *
-             *        HTTP/1.0 200 OK\r\n
-             *        Server: FileServer\r\n
-             *        \r\n
-             *        FILE CONTENTS HERE\r\n
-             *
-             *      If the specified file does not exist, you should return a
-             *      reply with an error code 404 Not Found. This reply should be
-             *      formatted as:
-             *
-             *        HTTP/1.0 404 Not Found\r\n
-             *        Server: FileServer\r\n
-             *        \r\n
-             *
-             * If you wish to do so, you are free to re-use code from
-             * MiniProject 2 to help with completing this MiniProject.
-             */
+            Thread thread = new Thread(
+
+                    () -> {
+
+                        try( BufferedReader reader = new BufferedReader(new InputStreamReader(accept.getInputStream()));
+                             BufferedWriter wtiter = new BufferedWriter(new OutputStreamWriter(accept.getOutputStream()))
+                        ) {
+
+                            String line = reader.readLine();
+                            String[] request = line.split(" ");
+
+                            String path = request[1];
+                            PCDPPath pcdpPath = new PCDPPath(path);
+                            String file = fs.readFile(pcdpPath);
+
+                            if (file != null) {
+                                wtiter.append("HTTP/1.0 200 OK\r\n");
+                                wtiter.append("Server: FileServer\r\n");
+                                wtiter.append("\r\n");
+                                wtiter.append(file);
+                                wtiter.flush();
+                            } else {
+                                wtiter.append("HTTP/1.0 404 Not Found\r\n");
+                                wtiter.append("Server: FileServer\r\n");
+                                wtiter.append("\r\n");
+                            }
+                        }catch (Exception e){
+                            throw new RuntimeException(e);
+                        }
+                        finally {
+
+                        }
+                    });
+
+            thread.start();
+
         }
     }
 }
